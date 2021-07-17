b8976adeee7e3a69d78ea7688d961e0133547c08
Darren Kessner <Darren.Kessner@marlborough.org>
2021-07-17 13:23:31 -0700

    added cat.js, not converting properly yet

---
+++ cat/cat.js
@@ -0,0 +1,38 @@
+
+class ArrayList extends Array {
+    constructor() {super(...[]);}
+    size() {return this.length;}
+    add(x) {this.push(x);}
+    get(i) {return this[i];}
+    remove(i) {this.splice(i,1);}
+}
+
+ let cat;
+ let mouse;
+ let mouse_x, mouse_y;
+
+ function preload() {
+     cat = loadImage("cat.png");
+     mouse = loadImage("mouse.png");
+ }
+
+ function setup() {
+     createCanvas(400, 400);
+     noCursor();
+     cat.resize(100, 100);
+     mouse.resize(50, 50);
+     moveMouse();
+ }
+
+ function moveMouse() {
+     mouse_x = random(50, width - 50);
+     mouse_y = random(50, height - 50);
+ }
+
+ function draw() {
+     background(100);
+     imageMode(CENTER);
+     image(cat, mouseX, mouseY);
+     image(mouse, mouse_x, mouse_y);
+     if (dist(mouseX, mouseY, mouse_x, mouse_y) < 50) moveMouse();
+ }
---
+++ cat/index.md
@@ -0,0 +1,17 @@
+
+
+<script src="../p5/p5.min.js"></script>
+<script src="cat.js"></script>
+
+# cat
+
+<main></main>
+
+Converted p5.js:
+```javascript
+{% include_relative cat.js %}```
+
+Original Processing:
+```java
+{% include_relative cat.pde %}```
+
