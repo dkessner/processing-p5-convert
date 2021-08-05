0209cef3f30580d1074a65e96b6f8a2648f88b66
Darren Kessner <Darren.Kessner@marlborough.org>
2021-08-04 20:05:51 -0700

    added error handling

--- js/processing-p5-convert-bootstrap.js
+++ js/processing-p5-convert-bootstrap.js
@@ -28,15 +28,22 @@ let codeArrays = [];
 
 function preload()
 {
-    let filenames = document.getElementById("ppconvert").getAttribute("src").split(/[\ \n\t]+/);
-
-    console.log("[processing-p5-convert-bootstrap] Reading .pde files:");
-
-    for (let filename of filenames)
+    try
     {
-        console.log(" " + filename);
-        let temp =  loadStrings(filename);
-        codeArrays.push(temp);
+        let filenames = document.getElementById("ppconvert").getAttribute("src").split(/[\ \n\t]+/);
+
+        console.log("[processing-p5-convert-bootstrap] Reading .pde files:");
+
+        for (let filename of filenames)
+        {
+            console.log(" " + filename);
+            let temp =  loadStrings(filename);
+            codeArrays.push(temp);
+        }
+    }
+    catch(e)
+    {
+        console.log("[processing-p5-convert-bootstrap] Caught error in preload()"); 
     }
 }
 
@@ -51,11 +58,14 @@ function setup()
     const codeOutput = ppconvert.transformProcessing(code);
     console.log(codeOutput); 
 
-    // run the p5.js code
-    // Note: this will redefine setup() (the function we're in)
+    if (codeOutput)
+    {
+        // run the p5.js code
+        // Note: this will redefine setup() (the function we're in)
 
-    window.eval(codeOutput); 
-    document.getElementById("defaultCanvas0").remove();
-    new p5();
+        window.eval(codeOutput); 
+        document.getElementById("defaultCanvas0").remove();
+        new p5();
+    }
 }
 
